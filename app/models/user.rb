271acbb1e19185a6edab262d49fe9a5430b8e224
issa0133 <19950625issa@gmail.com>
2022-01-08 05:17:26 +0000

    [Finish]応用課題５

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,25 +3,25 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-         
+
   attachment :profile_image, destroy: false
 
   validates :name, length: {maximum: 20, minimum: 2}, uniqueness: true
-  validates :introduction, length: {maximum: 50} 
+  validates :introduction, length: {maximum: 50}
 
   has_many :books, dependent: :destroy
   has_many :favorites, dependent: :destroy
   has_many :book_comments, dependent: :destroy
   has_many :relationships, class_name: "Relationship", foreign_key: "follower_id", dependent: :destroy
   has_many :followers, through: :relationships, source: :followed
-  
+
   has_many :reverse_of_relationships, class_name: "Relationship", foreign_key: "followed_id", dependent: :destroy
   has_many :followeds, through: :reverse_of_relationships, source: :follower
-  
+
   def is_followed_by?(user)
     reverse_of_relationships.find_by(follower_id: user.id).present?
   end
-  
+
   def self.looks(search, word)
     if search == "perfect_match"
       @user = User.where("name LIKE?", "#{word}")
@@ -35,5 +35,5 @@ class User < ApplicationRecord
       @user = User.all
     end
   end
-  
+
 end
